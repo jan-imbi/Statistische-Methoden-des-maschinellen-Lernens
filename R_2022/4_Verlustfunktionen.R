1b32b0f81a365d2a73b5c74fdc7db22e6436638b
Jan Meis <meis@imbi.uni-heidelberg.de>
2022-04-07 17:17:21 +0200

    VL 4

--- R_2022/4_Verlustfunktionen.R
+++ R_2022/4_Verlustfunktionen.R
@@ -11,9 +11,17 @@ plot(X_train, y_train, ylim = c(-25, 25))
 lin_mod <- lm(y_train ~ X_train)
 library(MASS)
 rob_mod <- rlm(y_train ~ X_train)
-pred_lin <- predict(lin_mod, data.frame(X_test = X_test))
+pred_lin <- predict(lin_mod, data.frame("(Intercept)" = 1, X_test = X_test))
 pred_rob <- predict(rob_mod, data.frame(X_test = X_test))
-res_lin <- mean((y_test - pred_lin)^2)
+pred2_lin <- lin_mod$coefficients[1] + lin_mod$coefficients[2] * X_test
+pred2_rob <- rob_mod$coefficients[1] + rob_mod$coefficients[2] * X_test
+
+mean((pred2_lin - y_test)^2)
+mean((pred2_rob - y_test)^2)
+
+
+
+xres_lin <- mean((y_test - pred_lin)^2)
 res_rob <- mean((y_test - pred_rob)^2)
 res_lin
 res_rob
