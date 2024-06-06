c40b8e6798066c42160487dfc096ab15c9b133d4
Jan Meis <meis@imbi.uni-heidelberg.de>
2024-06-06 13:03:28 +0200

    Stand 06.06.2024

--- R_2024/6_Resampling.R
+++ R_2024/6_Resampling.R
@@ -1,86 +1,14 @@
-# Aufgabe 1
-set.seed(12345)
+library(DescTools)
 library(evidence)
 data(fev)
-n <- nrow(fev)
-train <- sample(n, .6*n) # 60% of the data is used for training
-lm.fit <- lm(FEV ~ Age + Hgt,
-             data = fev,
-             subset = train)
-mean((fev$FEV-predict(lm.fit, fev))[-train]^2)
 
-set.seed(123456)
-data(fev)
-n <- nrow(fev)
-train <- sample(n, .6*n) # 60% of the data is used for training
-lm.fit <- lm(FEV ~ Age + Hgt,
-             data = fev,
-             subset = train)
-mean((fev$FEV-predict(lm.fit, fev))[-train]^2)
-
-set.seed(1234567)
-data(fev)
-n <- nrow(fev)
-train <- sample(n, .6*n) # 60% of the data is used for training
-lm.fit <- lm(FEV ~ Age + Hgt,
-             data = fev,
-             subset = train)
-mean((fev$FEV-predict(lm.fit, fev))[-train]^2)
-
-set.seed(12345)
-data(fev)
-n <- nrow(fev)
-train <- sample(n, .6*n) # 60% of the data is used for training
-lm.fit <- lm(FEV ~ Age + Hgt + Sex + Smoke,
-             data = fev,
-             subset = train)
-mean((fev$FEV-predict(lm.fit, fev))[-train]^2)
+index <- sample(nrow(fev), nrow(fev)*.6)
+modl1 <- lm(FEV ~ Age + Hgt, data = fev, subset = index)
+mean((predict(modl1, fev) - fev$FEV)[-index]^2)
 
 
-set.seed(123456)
-data(fev)
-n <- nrow(fev)
-train <- sample(n, .6*n) # 60% of the data is used for training
-lm.fit <- lm(FEV ~ Age + Hgt + Sex + Smoke,
-             data = fev,
-             subset = train)
-mean((fev$FEV-predict(lm.fit, fev))[-train]^2)
-
-set.seed(1234567)
-data(fev)
-n <- nrow(fev)
-train <- sample(n, .6*n) # 60% of the data is used for training
-lm.fit <- lm(FEV ~ Age + Hgt + Sex + Smoke,
-             data = fev,
-             subset = train)
-mean((fev$FEV-predict(lm.fit, fev))[-train]^2)
-
-# Aufgabe 2
-set.seed(12345)
 library(boot)
-library(TH.data)
-data(GBSG2)
-glm.fit <- glm(cens ~ horTh + age + menostat + tsize + tgrade + pnodes,
-               data=GBSG2, family = binomial(link = "logit"))
-cv.error <- cv.glm(GBSG2, glm.fit)
-cv.error$delta[1]
-cv.error5 <- cv.glm(GBSG2, glm.fit, K=5)
-cv.error5$delta[1]
+modl1 <- glm(FEV ~ Age + Hgt, data = fev)
 
-set.seed(12345)
-cv.error10 <- cv.glm(GBSG2, glm.fit, K=10)
-cv.error10$delta[1]
-cv.error50 <- cv.glm(GBSG2, glm.fit, K=50)
-cv.error50$delta[1]
+cv.glm(fev, modl1, K=10)
 
-# Aufgabe 3
-set.seed(12345)
-data(weightgain)
-theta.fct <- function(data, index){
-  return(coef(lm(weightgain ~ ., data=data, subset=index)))
-}
-theta_boot <- boot(weightgain, theta.fct, R=200)
-print(theta_boot)
-
-boot.ci(theta_boot)
-summary(lm(weightgain ~ ., data=weightgain))
