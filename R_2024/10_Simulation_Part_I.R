a1064e1b39bb55c206f282c66c5bec1e81049ac5
Jan Meis <meis@imbi.uni-heidelberg.de>
2024-06-20 10:45:26 +0200

    prepare 20.06.2024

---
+++ R_2024/10_Simulation_Part_I.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+## Parameter
+n_obs = c(10,50,100,200,300,400,500,600,700,800,900,1000,10000)
+n_sim = 1900
+set.seed(24062021)
+seed <- sample(1:10000000, replace = FALSE, size = n_sim)
+sig_result <- matrix(NA, nrow = n_sim, ncol = length(n_obs),
+                     dimnames = list(NULL, paste0("n_obs_",n_obs)))
+rejection <- data.frame(n_obs            = factor(n_obs),
+                        Rejection_Est    = NA,
+                        Rejection_Est_SE = NA)
+## Simulation
+for (i in 1:length(n_obs)) {
+  for (j in 1:n_sim) {
+    set.seed(seed[j])
+    ## Data Generation
+    A <- rnorm(n = n_obs[i]/2, mean = 5, sd = 2)
+    P <- rnorm(n = n_obs[i]/2, mean = 5, sd = 2)
+    ## Testen:
+    sig_result[j,i] <- t.test(x=A, y=P, alternative = "two.sided")$p.value <= 0.05
+  }
+  ## Ergebnisaufbereitung
+  rejection$Rejection_Est[i]    <- sum(sig_result[,i])/n_sim
+  rejection$Rejection_Est_SE[i] <- sqrt( (rejection$Rejection_Est[i] * (1-rejection$Rejection_Est[i]) ) / n_sim)
+}
+rejection
+
+rejection %>%
+  ggplot(aes(x=n_obs, y=Rejection_Est, group=1)) +
+  geom_point() + geom_line() + geom_hline(yintercept = 0.05) + lims(y=c(0,0.1))
+
+rejection %>%
+  ggplot(aes(x=n_obs, y=Rejection_Est_SE, group=1)) +
+  geom_point() + geom_line() + lims(y=c(0,0.01))
+
+rejection %>%
+  ggplot(aes(x=n_obs, y=Rejection_Est, group=1)) +
+  geom_point() + geom_line() + geom_hline(yintercept = 0.05) + lims(y=c(0,0.1)) +
+  geom_ribbon(aes(ymin = Rejection_Est - qnorm(.975)*Rejection_Est_SE,
+                  ymax = Rejection_Est + qnorm(.975)*Rejection_Est_SE,
+                  x=n_obs), alpha=0.2)
+
---
+++ R_2024/9_Variable_Selection.R
@@ -0,0 +1,133 @@
+library(MASS)
+library(dplyr)
+library(leaps)
+library(caret)
+
+# Aufgabe 1
+dat <- read.csv2("dat/heart.csv", sep=",") %>%
+  mutate(Herzkrank = factor(ifelse(Herzkrank == 0, "gesund", "krank")),
+         Geschlecht = factor(ifelse(Geschlecht == 0, "female", "male")),
+         Brustschmerz = factor(Brustschmerz),
+         Blutzucker = factor(ifelse(Blutzucker == 1, "erhoeht", "normal")),
+         EKG = factor(EKG),
+         Angina = factor(ifelse(Angina == 1, "ja", "nein")),
+         Depression1 = as.numeric(Depression1),
+         Depression2 = factor(Depression2)) %>%
+  dplyr::select(-Thal)
+str(dat)
+regfit_full <- leaps::regsubsets(Herzkrank ~ ., 
+                                 data = dat, 
+                                 nvmax = 16)
+results_bestsub <- summary(regfit_full)
+results_bestsub$outmat
+plot(regfit_full, scale = "bic")
+plot(regfit_full, scale = "adjr")
+which.max(results_bestsub$adjr2)
+plot(results_bestsub$bic,
+     main = "Bayesian information criterion over number of variables",
+     xlab = "Number of Variables",
+     ylab = "BIC",
+     type = "b")
+points(8, results_bestsub$bic[8], col="red", cex=2, pch=20)
+plot(results_bestsub$adjr2,
+     main = "Adjusted R^2 over number of variables",
+     xlab = "Number of Variables",
+     ylab = "Adjusted RSq",
+     type = "b")
+points(11, results_bestsub$adjr2[11], col="red", cex=2, pch=20)
+results_bestsub$which[11,]
+model_best_subset <- glm(Herzkrank ~ Geschlecht + Brustschmerz + 
+                           Blutdruck + Cholesterin + Herzschlag +
+                           Angina + Depression1 + Depression2 +
+                           Gefaesse,
+                         data = dat,
+                         family = "binomial")
+summary(model_best_subset)
+
+
+# Aufgabe 2
+
+# define null model 
+model_null <- glm(Herzkrank ~ 1, data = dat, family = "binomial")
+
+# perform forward selection
+results_FWS <- stepAIC(
+  model_null, 
+  direction = "forward",
+  scope = list(lower = ~ 1,
+               upper = Herzkrank ~ Alter + Geschlecht + 
+                 Brustschmerz + Blutdruck + Cholesterin + 
+                 Blutzucker + EKG + Herzschlag + Angina + 
+                 Depression1 + Depression2 + Gefaesse))
+
+# view results
+summary(results_FWS)
+
+
+# define full model
+model_full <- glm(Herzkrank ~ ., data = dat, family = "binomial")
+
+# perform backward selection
+results_BWS <- stepAIC(model_full, direction = "backward")
+
+# view results
+summary(results_BWS)
+
+
+# perform stepwise selection
+results_both <- stepAIC(model_full, direction = "both", trace = TRUE)
+
+# view results
+summary(results_both)
+
+# generate final model including selected variables
+model_final <- glm(Herzkrank ~ Geschlecht + Brustschmerz + 
+                     Blutdruck + Cholesterin + Herzschlag +
+                     Angina + Depression1 + Depression2 + Gefaesse,
+                   data = dat,
+                   family = "binomial")
+
+# 5-fache Kreuzvalidierung des finalen Modells
+set.seed(20042021)
+glm_best <- caret::train(Herzkrank ~ Geschlecht + Brustschmerz + Blutdruck + 
+                           Cholesterin + Herzschlag + Angina + Depression1 + 
+                           Depression2 + Gefaesse, 
+                         data = dat,
+                         method = "glm",
+                         trControl = trainControl(
+                           method="cv",
+                           number=5,
+                           classProbs=TRUE,
+                           summaryFunction = twoClassSummary
+                         ),
+                         metric = "ROC")
+
+roc_best <- pROC::roc(response = dat$Herzkrank=="krank", # observations
+                      predictor = predict(glm_best$finalModel), # predictions
+                      ci = TRUE,        # compute confidence interval
+                      plot = TRUE,      # create ROC plot
+                      print.auc = TRUE,
+                      main = "ROC curve of the final logistic regression model")
+
+
+# 5-fold cross-validation of the full model
+glm_full <- caret::train(Herzkrank ~ ., data = dat,
+                         method = "glm",
+                         trControl = trainControl(
+                           method="cv",
+                           number=5,
+                           classProbs=TRUE,
+                           summaryFunction = twoClassSummary
+                         ),
+                         metric = "ROC")
+
+roc_full <- pROC::roc(response = dat$Herzkrank=="krank", # observations
+                      predictor = predict(glm_full$finalModel), # predictions
+                      ci = TRUE,
+                      plot = TRUE,
+                      add = TRUE,       # add ROC curve to existing plot
+                      col = "darkgreen",
+                      print.auc.adj=c(0,5), # adjust position of AUC
+                      print.auc = TRUE,
+                      main = "ROC curve of the final(black) and full (green) model")
+
